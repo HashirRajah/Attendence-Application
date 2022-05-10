ba7f2244f84405d0e79e09ec4aee2cc314d7a642
Khizar Panchoo <khizarp@gmail.com>
2022-05-10 16:11:24 +0400

    create

--- Database/tables.sql
+++ Database/tables.sql
@@ -1,2 +1,23 @@
 CREATE DATABASE attendence_application_db;
 USE attendence_application_db;
+
+CREATE TABLE department
+(
+    deptId INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE modules
+(
+    moduleId INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE programs
+(
+    progId INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+    progType VARCHAR(255) UNIQUE,
+    deptId INT REFERENCES department,
+    degreeType VARCHAR(255) CHECK(degreeType IN ('BSc', 'MSc', 'PHd'))
+);
