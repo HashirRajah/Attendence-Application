c9d0885d1354b49afce8aca921d4e4b887d53324
Khizar Panchoo <khizarp@gmail.com>
2022-05-10 16:34:16 +0400

    create

--- Database/tables.sql
+++ Database/tables.sql
@@ -18,6 +18,44 @@ CREATE TABLE programs
     progId INT IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(255) UNIQUE,
     progType VARCHAR(255) UNIQUE,
-    deptId INT REFERENCES department,
+    deptId INT FOREIGN KEY REFERENCES department,
     degreeType VARCHAR(255) CHECK(degreeType IN ('BSc', 'MSc', 'PHd'))
+);
+
+CREATE TABLE students
+(
+    studId INT IDENTITY(1,1) PRIMARY KEY,
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    gender CHAR(1) CHECK(gender IN ('M', 'F')),
+    address VARCHAR(255),
+    email VARCHAR(255) CHECK(email LIKE '%___@___%'),
+    contact INT,
+    progId INT FOREIGN KEY REFERENCES programs,
+    password VARCHAR(255)
+);
+
+CREATE TABLE lecturer
+(
+    l_username INT IDENTITY(1,1) PRIMARY KEY,
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    gender CHAR(1) CHECK(gender IN ('M', 'F')),
+    address VARCHAR(255),
+    email VARCHAR(255) CHECK(email LIKE '%___@___%'),
+    contact INT,
+    type VARCHAR(255) CHECK(type IN ('Part-time', 'Full-time')),
+    password VARCHAR(255)
+);
+
+CREATE TABLE admin
+(
+    a_username INT IDENTITY(1,1) PRIMARY KEY,
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    gender CHAR(1) CHECK(gender IN ('M', 'F')),
+    address VARCHAR(255),
+    email VARCHAR(255) CHECK(email LIKE '%___@___%'),
+    contact INT,
+    password VARCHAR(255)
 );
