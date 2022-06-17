7e89b052389c5a17ef1b687a9b69ab725a19caf5
Khizar Panchoo <khizarp@gmail.com>
2022-06-17 15:53:11 +0400

    db connection

--- AttendanceApp/src/db/DatabaseConnection.java
+++ AttendanceApp/src/db/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class DatabaseConnection {
     public static void dbConnect() {
         String connectionString = new String(
-                "jdbc:sqlserver://localhost:1433;DatabaseName=company1;encrypt=true;trustServerCertificate=true;IntegratedSecurity=true");
+                "jdbc:sqlserver://localhost:1433;DatabaseName=attendence_application_db;encrypt=true;trustServerCertificate=true;IntegratedSecurity=true");
         //
         try {
             try (Connection db_connect = DriverManager.getConnection(connectionString)) {
