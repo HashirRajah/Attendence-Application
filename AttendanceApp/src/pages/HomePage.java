69f805a0b3a0fc2f92886b1c1106fccfa602f897
Khizar Panchoo <khizarp@gmail.com>
2022-07-01 21:26:09 +0400

    homepage

--- AttendanceApp/src/pages/HomePage.java
+++ AttendanceApp/src/pages/HomePage.java
@@ -9,7 +9,10 @@ import variables.*;
 
 public class HomePage extends JPanel {
     // attributes
-    private JLabel label;
+    private JLabel title;
+    private JPanel woohoo;
+    private ButtonStyle1 classBtn;
+    private String classStatus;
 
     // methods
     public HomePage(Theme theme) {
@@ -23,11 +26,24 @@ public class HomePage extends JPanel {
 
     public void setUpLabel(Theme theme) {
         if (Variables.loggedIn && Variables.userLoggedIn != null) {
-            this.label = new JLabel(
-                    "<html>Hello " + Variables.userLoggedIn.getFirstName() + "<br />Welcome back</html>");
-            label.setForeground(theme.getContentColor());
-            label.setFont(Variables.PAGES_TITLE2);
-            this.add(label, BorderLayout.NORTH);
+            title = new JLabel();
+            woohoo = new JPanel();
+            classStatus = "Woohoo.. No Class";
+            // title customization
+            title.setPreferredSize(new Dimension(this.getWidth(), 150));
+            title.setForeground(theme.getContentColor());
+            title.setText("<html>Hello " + Variables.userLoggedIn.getFirstName() + "<br />Welcome Back</html>");
+            title.setFont(Variables.PAGES_TITLE);
+            title.setVerticalAlignment(JLabel.CENTER);
+            title.setHorizontalAlignment(JLabel.CENTER);
+            this.add(title, BorderLayout.NORTH);
+
+            woohoo.setLayout(new GridLayout(1, 1));
+            woohoo.setBackground(theme.getMainColor());
+            classBtn = new ButtonStyle1(theme, theme.getMenuColor(), 3, classStatus);
+            woohoo.add(classBtn);
+            this.add(woohoo);
+
         }
     }
 
--- Database/tables.sql
+++ Database/tables.sql
@@ -361,3 +361,6 @@ VALUES
     ( 'BSonah', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' , 'lecturer'),
     ( 'PAppavoo', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' , 'lecturer'),
     ( 'VSeetohul', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' , 'lecturer');
+
+SELECT *
+FROM class
